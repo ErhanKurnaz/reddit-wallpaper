646dd89050f619d04da5ab75be02832c966a6a0c
Erhan Kurnaz <erhan@globalscreen.nl>
2021-07-16 22:11:21 +0200

    initial commit 2

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace reddit_wallpaper.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
